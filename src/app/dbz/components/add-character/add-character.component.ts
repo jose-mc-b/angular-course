b29220735e9bab9b1a76d34e52c59fcff7ec6d54
Jose Manuel <75644861+jose-mc-b@users.noreply.github.com>
2023-08-22 14:59:41 -0600

    id generation moved to services Id generation moved from Add Character so it's not responsible for that task

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -20,11 +20,10 @@ export class AddCharacterComponent {
 
 
   emitCharacter():void{
-    console.log(this.character);
 
     if (this.character.name.length ===0 ) return;
 
     this.onNewCharacter.emit(this.character);
-    this.character = {id: uuid(), name: ' ', power: 0};
+    this.character = {id: '', name: ' ', power: 0};
   }
 }
